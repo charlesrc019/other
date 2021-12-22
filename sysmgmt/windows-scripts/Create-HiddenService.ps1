27c815f305ab5792b5a040fff9650d0c86232537
Charles Christensen <30779702+charlesrc19@users.noreply.github.com>
2021-12-21 23:59:24 -0700

    Add files via upload

---
+++ sysmgmt/windows-scripts/Create-HiddenService.ps1
@@ -0,0 +1,46 @@
+﻿# Set variables.
+$NSSM_LOC = "C:\Users\Christensen\Downloads\nssm.exe"
+$SCRIPT_LOC = "C:\Users\Christensen\Downloads\Monitor-ComputerUsage.ps1"
+$BRANDS = @("Microsoft", "Windows", "Hyper-V", "Intel(R)", "Realtek", "Google", "Diagnostic", "Network")
+$TYPES = @("Device", "Communication", "Encoding", "License", "Support", "Data", "Policy")
+$EXTS = @("Extension", "Helper", "Runtime", "Broker", "Host", "Monitor", "Updater", "Service")
+
+# Create random names.
+$tmp1 = Get-ChildItem "$($env:WinDir)\System32\WindowsPowerShell\v1.0\Modules" -Directory | Get-Random
+$tmp2 = Get-Random $TYPES
+$tmp3 = Get-Random $EXTS
+$script_path = "$($tmp1.FullName)\$($tmp2)$($tmp3).ps1"
+$tmp1 = Get-Random $TYPES
+$tmp2 = Get-Random $EXTS
+$nssm_name = "$($tmp1)$($tmp2)"
+$nssm_path = "$($env:WinDir)\System32\$($nssm_name).exe"
+$tmp1 = Get-Random $BRANDS
+$tmp2 = Get-Random $TYPES
+$tmp3 = Get-Random $EXTS
+$svc_name = "$($tmp1) $($tmp2) $($tmp3)"
+$svc_descrip = Get-WmiObject win32_service | Get-Random
+
+# Move files.
+Copy-Item $NSSM_LOC -Destination $nssm_path -Force
+Copy-Item $SCRIPT_LOC -Destination $script_path -Force
+Set-ItemProperty -Path $script_path -Name IsReadOnly -Value True
+
+# Create service.
+$nssm = (Get-Command $nssm_name).Source
+$powershell = (Get-Command powershell).Source
+$arguments = '-ExecutionPolicy Bypass -NoProfile -File "{0}"' -f $script_path
+& $nssm install $svc_name $powershell $arguments | Out-Null
+& $nssm set $svc_name Description $svc_descrip.Description | Out-Null
+Start-Service $svc_name | Out-Null
+Get-Service $svc_name | Out-Null
+Write-Host "Hidden service created successfully!"
+
+# Clear sensitive parameters.
+$tmp1 = 0
+$tmp2 = 0
+$tmp3 = 0
+$script_path = 0
+$nssm_path = 0
+$nssm_name = 0
+$svc_name = 0
+$svc_descrip = 0
---
+++ sysmgmt/windows-scripts/Monitor-ComputerUsage.ps1
@@ -0,0 +1,75 @@
+﻿# Initialize variables.
+$LIMITED_HRS = @(0,1,2,3)
+$LIMITED_APPS = @("vlc","wmplayer","Microsoft.Photos","firefox","brave","chromium","opera","iexplore","taskmgr","mmc","IGCC","cmd","WinStore.App","powershell_ise") # "msedge","chrome
+
+$SLEEP_SECS = 1
+$SWITCHCHK_SECS = 60 * 2
+$URL = "http://www.chris-eng.com/status.txt"
+
+# Main.
+$hr = $null
+$switch_limit = $false
+$secs = $SWITCHCHK_SECS
+while ($true) {
+
+    # Check time limits.
+    $time_limit = $false
+    $hr = (Get-Date).Hour
+    foreach ($limited_hr in $LIMITED_HRS) {
+        if ($hr -eq $limited_hr) {
+            $time_limit = $true
+            Stop-Computer -Force
+        }
+    }
+
+    # Verify that A2Y is on. Force computer to turn off if not.
+    $i = 0
+    while ($true) {
+        $status = Get-Process | Where {$_.ProcessName -Like "Accountable2You"}
+        if ($status.Length -lt 1) {
+            $i += 1
+            if ($i -gt ($SWITCHCHK_SECS/4)) {
+                Stop-Computer -Force
+            }
+            Start-Sleep -Seconds $SLEEP_SECS
+        }
+        else {
+            break
+        }
+    }
+
+    # Check kill switch.
+    if ((-not $time_limit) -and ($secs -ge $SWITCHCHK_SECS)) {
+        $secs = 0
+
+        # Create request.
+        $rqst = [system.Net.WebRequest]::Create($URL)
+        $resp = $null
+        try {
+            $resp = $rqst.GetResponse()
+        } 
+        catch [System.Net.WebException] {
+            $resp = $_.Exception.Response
+        }
+
+        # Eval response.
+        if ($resp.StatusCode -eq "NotFound") {
+            $switch_limit = $false
+        }
+        else {
+            $switch_limit = $true
+        }
+    }
+    
+    # Implement limits.
+    if ($time_limit -or $switch_limit) {
+        foreach ($limited_app in $LIMITED_APPS) {
+            Stop-Process -Name $limited_app -Force -ErrorAction SilentlyContinue
+        }
+    }
+
+    # Delay.
+    Start-Sleep -Seconds $SLEEP_SECS
+    $secs += $SLEEP_SECS
+
+}
