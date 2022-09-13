f91c2f91d1cdf463c096ad456a9c7db6beee4517
Charles Christensen <30779702+charlesrc019@users.noreply.github.com>
2022-09-13 14:22:37 -0600

    Add files via upload

--- sysmgmt/unix-scripts/BlockComputerUsage.sh
+++ sysmgmt/unix-scripts/BlockComputerUsage.sh
@@ -14,10 +14,10 @@
 #
 
 # Initialize.
+main_user="cchristensen"
 echo "Starting BlockComputerUsage.sh on `date` as `whoami`..." > /Library/Logs/BlockComputerUsage.log
 echo " " >> /Library/Logs/BlockComputerUsage.log
 run_once=0
-run_fivesecs=0
 
 # Monitor tasks.
 while [ 1 ]
@@ -35,10 +35,10 @@ do
     # Prevent extra users.
     dscl . list /Users | grep -v "^_" | while read -r line
     do
-        if [ $line != "cchristensen" ] &&
-           [ $line != "daemon"       ] &&
-           [ $line != "nobody"       ] &&
-           [ $line != "root"         ]
+        if [ $line != $main_user ] &&
+           [ $line != "daemon"   ] &&
+           [ $line != "nobody"   ] &&
+           [ $line != "root"     ]
         then
             echo "> Deleting user '$line'." >> /Library/Logs/BlockComputerUsage.log
             dscl . -delete /Users/$line >> /Library/Logs/BlockComputerUsage.log
@@ -48,9 +48,9 @@ do
     # Prevent extra user folders.
     ls /Users | while read -r line
     do
-        if [ $line != "cchristensen" ] &&
-           [ $line != "Shared"       ] &&
-           [ $line != ".localized"   ]
+        if [ $line != $main_user   ] &&
+           [ $line != "Shared"     ] &&
+           [ $line != ".localized" ]
         then
             echo "> Deleting user folder '$line'." >> /Library/Logs/BlockComputerUsage.log
             rm -rf /Users/$line >> /Library/Logs/BlockComputerUsage.log
@@ -62,22 +62,19 @@ do
     chown root:wheel $fullpath
     chflags schg $fullpath
     chflags uchg $fullpath
-    chown root:wheel /Library/LaunchDaemons/com.charlesrc19.BlockComputerUsage.plist
-    chflags schg /Library/LaunchDaemons/com.charlesrc19.BlockComputerUsage.plist
-    chflags uchg /Library/LaunchDaemons/com.charlesrc19.BlockComputerUsage.plist
-    chown root:wheel /private/etc/hosts
-    chflags schg /private/etc/hosts
-    chflags uchg /private/etc/hosts
+    chown root:wheel /Library/LaunchDaemons/com.user.BlockComputerUsage.plist
+    chflags schg /Library/LaunchDaemons/com.user.BlockComputerUsage.plist
+    chflags uchg /Library/LaunchDaemons/com.user.BlockComputerUsage.plist
+    chown root:wheel /etc/hosts
+    chflags schg /etc/hosts
+    chflags uchg /etc/hosts
 
-    # Run five-second tasks.
-    sec=$(date +%S)
-    if (( 10#$run_fivesecs < 10#$sec ))
+    # Run one-time tasks.
+    if [ $run_once == 0 ]
     then
-        let run_fivesecs=$sec+5
-        if (( 10#$run_fivesecs > 60 ))
-        then
-            let run_fivesecs=$run_fivesecs-60
-        fi
+        run_once=1
+
+        echo "> Locking system modifications..." >> /Library/Logs/BlockComputerUsage.log
 
         # Prevent extra user creation.
         security -q authorizationdb read  system.preferences.accounts > /tmp/system.preferences.accounts.plist
@@ -94,32 +91,30 @@ do
         defaults write /tmp/system.preferences.security.plist group wheel > /dev/null
         security -q authorizationdb write system.preferences.security < /tmp/system.preferences.security.plist
 
-    fi
-
-    # Run one-time tasks.
-    if [ $run_once == 0 ]
-    then
-        run_once=1
+        echo "> Complete!" >> /Library/Logs/BlockComputerUsage.log
+        echo " " >> /Library/Logs/BlockComputerUsage.log
 
         # Update hosts.
-        if [ $dow = "Monday" ]
+        if [ $dow == "Monday" ]
         then
             echo "> Updating hosts file..." >> /Library/Logs/BlockComputerUsage.log
-            sleep 30 > /dev/null
-            chflags noschg /private/etc/hosts
-            chflags nouchg /private/etc/hosts
-            rm /private/etc/hosts
-            echo "> Downloading updated hosts file..." >> /Library/Logs/BlockComputerUsage.log
-            curl -m 30 -s --retry 3 --retry-delay 1 --connect-timeout 3 https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts -o /private/etc/hosts
-            echo "> New host file downloaded." >> /Library/Logs/BlockComputerUsage.log
-            cat /Users/cchristensen/Documents/hosts_custom.txt >> /private/etc/hosts
+            chflags noschg /etc/hosts
+            chflags nouchg /etc/hosts
+            rm /etc/hosts
+            cp /Users/cchristensen/Documents/hosts.txt /etc/hosts
+            chown root:wheel /etc/hosts
+            chmod 644 /etc/hosts
             killall -HUP mDNSResponder
             echo "> Complete!" >> /Library/Logs/BlockComputerUsage.log
             echo " " >> /Library/Logs/BlockComputerUsage.log
         fi
 
+        # Delay to ensure initialization.
+        #sleep 30 > /dev/null
+
     fi
 
-    sleep 0.1 > /dev/null
+    sleep 0.5 > /dev/null
 
 done
+
