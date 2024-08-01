9f01ce2e649d60cc00e6b8064004bc212981c5b6
Charles Christensen <30779702+charlesrc019@users.noreply.github.com>
2024-08-01 11:06:06 -0600

    Add files via upload

---
+++ sysmgmt/unix-scripts/hosts_cache.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+#curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts > /Library/Scripts/User/hosts_cache.txt
+curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts > /Library/Scripts/User/hosts_cache.txt
