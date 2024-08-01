a3e7cfd68913b3ff0f91540eb7271b1dbe37b071
Charles Christensen <30779702+charlesrc019@users.noreply.github.com>
2024-08-01 11:05:31 -0600

    Update BlockComputerUsage.sh

--- sysmgmt/unix-scripts/BlockComputerUsage.sh
+++ sysmgmt/unix-scripts/BlockComputerUsage.sh
@@ -14,14 +14,25 @@
 #
 
 # Initialize.
-main_user="cchristensen"
+main_user="charles"
 echo "Starting BlockComputerUsage.sh on `date` as `whoami`..." > /Library/Logs/BlockComputerUsage.log
 echo " " >> /Library/Logs/BlockComputerUsage.log
+run_hourly=0
 run_once=0
 
-# Monitor tasks.
+# Monitor tasks continuously.
 while [ 1 ]
 do
+    dow=$(date +%A)
+    tm=$(date +%H%M)
+
+    # Prevent night-time computer usage.
+    # blocks:  <9:45am                    >2pm              any Saturday              any Sunday
+    if (( 10#$tm < 945 )) || (( 10#$tm >= 1400 )) || [ $dow == "Saturday" ] || [ $dow == "Sunday" ]
+    then
+        echo "> Shutting down." >> /Library/Logs/BlockComputerUsage.log
+        shutdown -h now  >> /Library/Logs/BlockComputerUsage.log
+    fi
 
     # Prevent user from modifying this file, its startup, or hosts.
     [[ $0 = /* ]] && fullpath=$0 || fullpath=$PWD/${0#./}
@@ -29,70 +40,16 @@ do
     chflags uchg $fullpath
     chflags schg /Library/LaunchDaemons/com.user.BlockComputerUsage.plist
     chflags uchg /Library/LaunchDaemons/com.user.BlockComputerUsage.plist
-
-    # Prevent night-time computer usage. (blocks 5pm - 9am & Sundays)
-    dow=$(date +%A)
-    hr=$(date +%H)
-    if (( 10#$hr < 9 )) || (( 10#$hr >= 17 )) || [ $dow == "Sunday" ]
-    then
-        echo "> Shutting down." >> /Library/Logs/BlockComputerUsage.log
-        shutdown -h now  >> /Library/Logs/BlockComputerUsage.log
-    fi
-
-    # Run one-time tasks.
-    if [ $run_once == 0 ]
-    then
-        run_once=1
-
-        echo "> Locking system modifications..." >> /Library/Logs/BlockComputerUsage.log
-
-        # Prevent extra user creation.
-        security -q authorizationdb read  system.preferences.accounts > /tmp/system.preferences.accounts.plist
-        defaults write /tmp/system.preferences.accounts.plist group wheel > /dev/null
-        security -q authorizationdb write system.preferences.accounts < /tmp/system.preferences.accounts.plist
-
-        # Prevent time modification.
-        security -q authorizationdb read  system.preferences.datetime > /tmp/system.preferences.datetime.plist
-        defaults write /tmp/system.preferences.datetime.plist group wheel > /dev/null
-        security -q authorizationdb write system.preferences.datetime < /tmp/system.preferences.datetime.plist
-
-        # Prevent screen recording modification.
-        security -q authorizationdb read  system.preferences.security > /tmp/system.preferences.security.plist
-        defaults write /tmp/system.preferences.security.plist group wheel > /dev/null
-        security -q authorizationdb write system.preferences.security < /tmp/system.preferences.security.plist
-
-        echo "> Complete!" >> /Library/Logs/BlockComputerUsage.log
-        echo " " >> /Library/Logs/BlockComputerUsage.log
-
-        # Update hosts.
-        chflags schg /etc/hosts
-        chflags uchg /etc/hosts
-        if [ $dow == "Monday" ]
-        then
-            echo "> Updating hosts file..." >> /Library/Logs/BlockComputerUsage.log
-            chflags noschg /etc/hosts
-            chflags nouchg /etc/hosts
-            rm /etc/hosts
-            cp /Users/cchristensen/Documents/hosts.txt /etc/hosts
-            chown root:wheel /etc/hosts
-            chmod 644 /etc/hosts
-            killall -HUP mDNSResponder
-            echo "> Complete!" >> /Library/Logs/BlockComputerUsage.log
-            echo " " >> /Library/Logs/BlockComputerUsage.log
-        fi
-
-        # Delay to ensure initialization.
-        sleep 10 > /dev/null
-
-    fi
+    chflags schg /etc/sudoers
+    chflags uchg /etc/sudoers
 
     # Prevent extra users.
     dscl . list /Users | grep -v "^_" | while read -r line
     do
-        if [ $line != $main_user ] &&
-           [ $line != "daemon"   ] &&
-           [ $line != "nobody"   ] &&
-           [ $line != "root"     ]
+        if [ "$line" != $main_user ] &&
+           [ "$line" != "daemon"   ] &&
+           [ "$line" != "nobody"   ] &&
+           [ "$line" != "root"     ]
         then
             echo "> Deleting user '$line'." >> /Library/Logs/BlockComputerUsage.log
             dscl . -delete /Users/$line >> /Library/Logs/BlockComputerUsage.log
@@ -102,73 +59,144 @@ do
     # Prevent extra user folders.
     ls /Users | while read -r line
     do
-        if [ $line != $main_user   ] &&
-           [ $line != "Shared"     ] &&
-           [ $line != ".localized" ]
+        if [ "$line" != $main_user   ] &&
+           [ "$line" != "Shared"     ] &&
+           [ "$line" != ".localized" ]
         then
             echo "> Deleting user folder '$line'." >> /Library/Logs/BlockComputerUsage.log
             rm -rf /Users/$line >> /Library/Logs/BlockComputerUsage.log
         fi
     done
 
-    # Prevent unmonitored browser use.
-    tmp=$(ps aux)
-    if (( 10#$( echo $tmp | grep -c -i Edge.app ) > 0 ))
+    # Prevent enabling root user.
+    if dscl . -read /Users/root Password | grep "\*\*" 
     then
-        echo "> Killing Microsoft Edge browser." >> /Library/Logs/BlockComputerUsage.log
-        pkill -f -i Edge.app -9 > /dev/null
-    fi
-    if (( 10#$( echo $tmp | grep -c -i firefox ) > 0 ))
-    then
-        echo "> Killing Firefox browser." >> /Library/Logs/BlockComputerUsage.log
-        pkill -f -i firefox -9 > /dev/null
-    fi
-    if (( 10#$( echo $tmp | grep -c -i brave ) > 0 ))
-    then
-        echo "> Killing Brave browser." >> /Library/Logs/BlockComputerUsage.log
-        pkill -f -i brave -9 > /dev/null
-    fi
-    if (( 10#$( echo $tmp | grep -c -i opera ) > 0 ))
-    then
-        echo "> Killing Opera browser." >> /Library/Logs/BlockComputerUsage.log
-        pkill -f -i opera -9 > /dev/null
-    fi
-    if (( 10#$( echo $tmp | grep -c -i onion ) > 0 ))
-    then
-        echo "> Killing Onion browser." >> /Library/Logs/BlockComputerUsage.log
-        pkill -f -i onion -9 > /dev/null
-    fi
-    if (( 10#$( echo $tmp | grep -c -i chromium ) > 0 ))
-    then
-        echo "> Killing Chromium browser." >> /Library/Logs/BlockComputerUsage.log
-        pkill -f -i chromium -9 > /dev/null
-    fi
-    if (( 10#$( echo $tmp | grep -c -i vivaldi ) > 0 ))
-    then
-        echo "> Killing Vivaldi browser." >> /Library/Logs/BlockComputerUsage.log
-        pkill -f -i vivaldi -9 > /dev/null
-    fi
-    if (( 10#$( echo $tmp | grep -c -i browser ) > 0 ))
-    then
-        echo "> Killing other browser." >> /Library/Logs/BlockComputerUsage.log
-        pkill -f -i browser -9 > /dev/null
+        echo "> Disabling root user." >> /Library/Logs/BlockComputerUsage.log
+        dscl . delete /Users/root AuthenticationAuthority
+        dscl . -create /Users/root UserShell /usr/bin/false
+        dscl . -create /Users/root Password '*'
     fi
 
-    # Prevent unmonitored DNS. (Lock to CleanBrowsing Family)
-    tmp1=$(scutil --dns | grep nameserver | grep -c 185.228.168.168)
-    tmp2=$(scutil --dns | grep nameserver | grep -c 185.228.169.168)
+    # Prevent unmonitored DNS. (Lock to CleanBrowsing Family.)
+    tmp1=$(scutil --dns | grep nameserver | grep -c -e 185.228.168.168 -e 127.0.0.1)
+    tmp2=$(scutil --dns | grep nameserver | grep -c -e 185.228.169.168 -e 127.0.0.1)
     tmp3=$(scutil --dns | grep -c nameserver)
     if (( 10#$tmp1 < 2 )) || (( 10#$tmp2 < 2 )) || (( 10#$tmp3 > 4 ))
     then
-        echo $tmp1
-        echo $tmp2
-        echo $tmp3
         echo "> Resetting DNS servers." >> /Library/Logs/BlockComputerUsage.log
         networksetup -setdnsservers Wi-Fi 185.228.168.168 185.228.169.168 > /dev/null
         networksetup -setdnsservers Ethernet 185.228.168.168 185.228.169.168 > /dev/null
     fi
 
-    sleep 1 > /dev/null
+    # Prevent unmonitored hosts.
+    tmp1=0
+    if [ -f /etc/hosts ]
+    then
+        tmp1=$(wc -l /etc/hosts | awk '{print $1}')
+    fi
+    if (( 10#$tmp1 < 100 ))
+    then
+        echo "> Replacing blank hosts file." >> /Library/Logs/BlockComputerUsage.log
+        if [ -f /etc/hosts ]
+        then
+            chflags noschg /etc/hosts
+            chflags nouchg /etc/hosts
+            rm /etc/hosts
+        fi
+        cp /Library/Scripts/User/hosts_cache.txt /etc/hosts
+        cat /Library/Scripts/User/hosts_custom.txt >> /etc/hosts
+        chown root:wheel /etc/hosts
+        chmod 644 /etc/hosts
+        killall -HUP mDNSResponder
+    fi
+
+    # Prevent use of Safari or Directory Utility.
+    uid=$(id -u "$main_user")
+    launchctl asuser $uid osascript -e "tell application \"Safari\" to quit" > /dev/null
+    launchctl asuser $uid osascript -e "tell application \"Directory Utility\" to quit" > /dev/null
+
+    # Run hourly tasks. Also run on script startup.
+    if ((( (10#$tm % 100) == 0 )) && (( 10#$run_hourly != 10#$tm ))) || [ $run_once == 0 ]
+    then
+        run_hourly=$tm
+
+        echo "> Locking system modifications..." >> /Library/Logs/BlockComputerUsage.log
+
+        # Prevent extra user creation.
+        security -q authorizationdb read system.preferences.accounts > /tmp/system.preferences.accounts.plist
+        defaults write /tmp/system.preferences.accounts.plist group wheel > /dev/null
+        security -q authorizationdb write system.preferences.accounts < /tmp/system.preferences.accounts.plist
+
+        # Prevent time modification.
+        security -q authorizationdb read system.preferences.datetime > /tmp/system.preferences.datetime.plist
+        defaults write /tmp/system.preferences.datetime.plist group wheel > /dev/null
+        security -q authorizationdb write system.preferences.datetime < /tmp/system.preferences.datetime.plist
+
+        # Prevent screen recording modification.
+        security -q authorizationdb read system.preferences.security > /tmp/system.preferences.security.plist
+        defaults write /tmp/system.preferences.security.plist group wheel > /dev/null
+        security -q authorizationdb write system.preferences.security < /tmp/system.preferences.security.plist 
+
+        # Prevent directory permission modification and root enable.
+        security -q authorizationdb read system.services.directory.configure > /tmp/system.services.directory.configure.plist
+        defaults write /tmp/system.services.directory.configure.plist group wheel > /dev/null
+        security -q authorizationdb write system.services.directory.configure < /tmp/system.services.directory.configure.plist
+        security -q authorizationdb read system.services.directory > /tmp/system.services.directory.plist
+        defaults write /tmp/system.services.directory.plist group wheel > /dev/null
+        security -q authorizationdb write system.services.directory < /tmp/system.services.directory.plist
+
+        echo "> + Complete!" >> /Library/Logs/BlockComputerUsage.log
+
+        # Flush the DNS cache.
+        echo "> Flushing the DNS cache..." >> /Library/Logs/BlockComputerUsage.log
+        killall -HUP mDNSResponder
+        echo "> + Complete!" >> /Library/Logs/BlockComputerUsage.log
+
+        # Insure root is disabled.
+        echo "> Locking-down root account..." >> /Library/Logs/BlockComputerUsage.log
+        dscl . delete /Users/root AuthenticationAuthority
+        dscl . -create /Users/root UserShell /usr/bin/false
+        dscl . -create /Users/root Password '*'
+        echo "> + Complete!" >> /Library/Logs/BlockComputerUsage.log
+
+        echo "> Hour $tm tasks complete!" >> /Library/Logs/BlockComputerUsage.log
+    fi
+
+    # Run one-time tasks.
+    if [ $run_once == 0 ]
+    then
+        run_once=1
+
+        # Update hosts.
+        if [ -f /etc/hosts ]
+        then
+            chflags schg /etc/hosts
+            chflags uchg /etc/hosts
+        fi
+        if [ $dow == "Monday" ] && (( 10#$tm < 1200 ))
+        then
+            echo "> Updating hosts file..." >> /Library/Logs/BlockComputerUsage.log
+            if [ -f /etc/hosts ]
+            then
+                chflags noschg /etc/hosts
+                chflags nouchg /etc/hosts
+                rm /etc/hosts
+            fi
+            cp /Library/Scripts/User/hosts_cache.txt /etc/hosts
+            cat /Library/Scripts/User/hosts_custom.txt >> /etc/hosts
+            chown root:wheel /etc/hosts
+            chmod 644 /etc/hosts
+            killall -HUP mDNSResponder
+            echo "> + Complete!" >> /Library/Logs/BlockComputerUsage.log
+        fi
+
+        # Delay to ensure initialization.
+        echo "> Waiting for initialization..." >> /Library/Logs/BlockComputerUsage.log
+        sleep 20 > /dev/null
+        echo "> + Complete!" >> /Library/Logs/BlockComputerUsage.log
+        echo " " >> /Library/Logs/BlockComputerUsage.log
+    fi
+
+    sleep 0.5 > /dev/null
 
 done
-
